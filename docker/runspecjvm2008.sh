a8d537d8cadf47c624bf76a792da2dca8b5b5284
Wes Lloyd <wlloyd@uw.edu>
2018-05-16 20:17:10 -0700

    update

--- docker/runspecjvm2008.sh
+++ docker/runspecjvm2008.sh
@@ -1,4 +1,5 @@
 # Simulates COLD run on Docker
 sudo docker build -t specjvm2008 .
+#contid=`sudo docker run -m 512m --cpus=3.0 -d --rm specjvm2008`
 contid=`sudo docker run -d --rm specjvm2008`
 time sudo docker exec -it $contid bash -c "cd specjvm2008 ; java Main scimark.fft.small 2 1 2 3"
