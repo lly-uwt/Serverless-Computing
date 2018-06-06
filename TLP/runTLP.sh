570ec4b7823e3cfa49c28e07bfd22c3a7b74eb97
lly-uwt <lanly@uw.edu>
2018-06-06 14:41:45 -0700

    update TLP script

--- TLP/runTLP.sh
+++ TLP/runTLP.sh
@@ -1,22 +1,38 @@
 #---run this at 1st
-# cd ..; mvn clean package
-# cd TLP
-# sudo docker stop tlp-container
-# sudo docker rmi tlp
-# sudo docker build -t tlp .
-# sudo docker run -it -d --rm --name=tlp-container tlp
+cd ..; mvn clean package
+cd TLP
+sudo docker stop tlp-container
+sudo docker rmi tlp
+sudo docker build -t tlp .
+sudo docker run -it -d --rm --name=tlp-container tlp
 #---
 
 # time sudo docker exec -it tlp-container bash -c "cd specjvm2008 ; java Main scimark.fft.small 2 1 2 3" > ex-out.txt
 # cat ex-out.txt
 # whole=`cat ex-out.txt`
 
-benchmarks=(scimark.fft.small scimark.fft.large)
+benchmarks=(derby
+ compress\
+ mp3gaudio\
+ scimark.fft.large\
+ scimark.fft.small\
+ scimark.lu.large\
+ scimark.lu.small\
+ scimark.sor.large\
+ scimark.sor.small\
+ scimark.sparse.large\
+ scimark.sparse.small\
+ scimark.monte_carlo\
+ serial\
+ sunflow\ 
+ xml.tranform\
+ xml.validation\
+)
+# sunflow and xml transform need fix
 cpuSetting=(1 2)
 
 for bm in ${benchmarks[@]}; do
     echo 'uuid,uptime,bmName,#cpu,ops,duration' > $bm.csv
-   
     for cpu in ${cpuSetting[@]}; do
         echo $bm $cpu
         sudo docker update --cpus=$cpu tlp-container
