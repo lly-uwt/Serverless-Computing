521ab81e45a22fdc6406c3a00bd94f293ed40a18
lly-uwt <lanly@uw.edu>
2018-09-26 21:09:56 -0700

    rename cpu-load-js -> cpu-load-ps-js, add bash-exe-js

---
+++ bash-exe-js/functionHandler.js
@@ -0,0 +1,56 @@
+const { exec } = require("child_process")
+let infos
+let timeup
+let duration = 5000
+let step = 500
+
+exports.handler = (event, context, callback) => {
+  infos = []
+  timeup = false
+  getInfo()
+
+  setTimeout(() => {
+    callback(null, infos)
+    timeup = true
+  }, duration) //(error, success)
+}
+
+exports.run = wantOutputFile => {
+  this.handler(null, null, (error, result) => {
+    if (error) console.error(error)
+    console.log(JSON.stringify(result))
+
+    if (wantOutputFile)
+      require("fs").writeFile("output.json", result, err => {
+        if (err) console.error(err)
+        console.log("output.json created")
+      })
+  })
+}
+
+function getInfo() {
+  let count = 0
+  const id = setInterval(() => {
+    if (timeup) {
+      clearInterval(id)
+      return
+    }
+    let procsArr = []
+    const procs = exec(
+      'bash -c "(sleep 1;echo 1) | TERM=xterm script -c top & sleep 2;killall script" | grep ^%Cpu',
+      (error, stdout, stderr) => {
+        let str = stdout
+        procsArr.push(str)
+        if (error !== null) {
+          str = error
+          procsArr.push(str)
+        }
+      }
+    )
+    procs.on(
+      "close",
+      existCode => infos.push({ index: (count = count + 1), data: procsArr }),
+      step
+    )
+  })
+}
